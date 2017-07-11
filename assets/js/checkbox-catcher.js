fa87a96e6f0974fb0c8103485bedfb7b2ff2a2dc
George Vikhrev <georgevikhrev@gmail.com>
2017-07-11 14:03:20 +0300

    refactored checkboxes

--- assets/js/checkbox-catcher.js
+++ assets/js/checkbox-catcher.js
@@ -1,22 +1,4 @@
 jQuery(document).ready(function($) {
-	
-	$("#ferryCheck").attr("checked", false);
-
-	// Switch section
-	$("a", '.mainmenu').click(function() 
-	{
-		if( ! $(this).hasClass('active') ) { 
-			current_item = this;
-			// close all visible divs with the class of .section
-			$('.section:visible').fadeOut( section_hide_time, function() { 
-				$('a', '.mainmenu').removeClass( 'active' );  
-				$(current_item).addClass( 'active' );
-				var new_section = $( $(current_item).attr('href') );
-				new_section.fadeIn( section_show_time );
-			} );
-		}
-		return false;
-	});
 
 	$("#transferCheck").click(function(){
 		var ferry = $("#ferryCheck");
@@ -26,7 +8,6 @@ jQuery(document).ready(function($) {
 		}
 		else{
 			ferry[0].checked = false;
-		}
-		
+		}		
     });
 });

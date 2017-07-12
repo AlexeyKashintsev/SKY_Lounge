2e5fe419201ecbd6323d1baddabbae64ab16454c
George Vikhrev <georgevikhrev@gmail.com>
2017-07-13 01:04:57 +0300

    Completed "Подача заявки"

--- assets/js/checkbox-catcher.js
+++ assets/js/checkbox-catcher.js
@@ -10,4 +10,13 @@ jQuery(document).ready(function($) {
 			ferry[0].checked = false;
 		}		
     });
+	
+	$("#oferta", "input:checkbox").click(function(){
+		btn = $("#btnSend");
+		if ($("#oferta")[0].checked == false){
+			btn[0].disabled = true;
+		} else{
+			btn[0].disabled = false;
+		};
+	});
 });

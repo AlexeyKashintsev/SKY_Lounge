c5c3da883e4e2a3bd312e50b11d1e1197edfd7d1
George Vikhrev <georgevikhrev@gmail.com>
2017-07-14 21:48:19 +0300

    Dynamic fields for members (chleny) and calculating price

---
+++ assets/js/dynamicfields.js
@@ -0,0 +1,61 @@
+jQuery(document).ready(function($) {
+    var ticketPrice = 1000, transferPrice = 800, ferryPrice = 400, numOfPeople = 1;
+    var price;
+    checkPrice();
+    
+    $(".priceCheck").click(function() {
+        checkPrice();   
+     });
+    
+    function checkPrice() {
+        if (price) price.remove();
+        var sum = +1000;
+            if ($('#transferCheck')[0].checked == true) sum += +($('#transferCheck')[0].value);
+            if ($('#ferryCheck')[0].checked == true) sum += +($('#ferryCheck')[0].value);
+        sum = sum * +($("#selectPeople").val());  
+        price = $('<b/>', {
+          id : totalPrice
+          }).html(sum).appendTo($('#containerPrice'));
+    }
+    
+    $("#selectPeople").change(function(){
+        checkPrice();
+        var curNum = $("#selectPeople").val();
+        if (numOfPeople < curNum ){
+            for (var i = +numOfPeople + 1; i<=curNum; i++){
+                addDynamicExtraField(i);
+            }
+        }
+        else if (numOfPeople > curNum){
+            for (var i = numOfPeople; i > curNum; i--){
+                $(".DynamicExtraField"+i).remove();
+            }
+        }
+        numOfPeople = curNum;
+    })
+        
+    function addDynamicExtraField(participant) {
+        var div = $('<div/>', {
+            'class' : 'DynamicExtraField' + participant
+        }).appendTo($('#customerBlock'));
+  //      var div = $('#customerBlock');
+        
+//        $('<br/>').appendTo(div);
+        $('<h3/>').html('Участник ' + participant).appendTo(div);
+        
+        $('<p/>').html("Имя: ").appendTo(div);
+        $('<input/>', {
+            id : 'name'+participant,
+            name : 'name'+participant,
+            type : 'text'
+        }).appendTo(div);
+        
+        $('<p/>').html("Фамилия: ").appendTo(div);
+        
+        $('<input/>', {
+            id : 'surname'+participant,
+            name : 'surname'+participant,
+            type : 'text'
+        }).appendTo(div);
+    }
+})

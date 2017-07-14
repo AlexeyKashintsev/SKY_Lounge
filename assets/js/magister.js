0a8932377d086541063a4f9c223acc3ee24a0b51
Alexey Kashintsev <ak@altsoft.biz>
2017-07-14 20:04:53 +0400

    changed magister js

--- assets/js/magister.js
+++ assets/js/magister.js
@@ -6,40 +6,42 @@ var section_hide_time = 700;
 var section_show_time = 700;
 
 // jQuery stuff
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-	// Switch section
-	$("a", '.mainmenu').click(function() 
-	{
-		if( ! $(this).hasClass('active') ) { 
-			current_item = this;
-			// close all visible divs with the class of .section
-			$('.section:visible').fadeOut( section_hide_time, function() { 
-				$('a', '.mainmenu').removeClass( 'active' );  
-				$(current_item).addClass( 'active' );
-				var new_section = $( $(current_item).attr('href') );
-				new_section.fadeIn( section_show_time );
-			} );
-		}
-		return false;
-	});
+    // Switch section
+    $("a", '.mainmenu').click(function () {
+        if (!$(this).hasClass('active')) {
+            current_item = this;
+            // close all visible divs with the class of .section
+            $('.section:visible').fadeOut(section_hide_time, function () {
+                $('a', '.mainmenu').removeClass('active');
+                $(current_item).addClass('active');
+                var new_section = $($(current_item).attr('href'));
+                new_section.fadeIn(section_show_time);
+            });
+        }
+        return false;
+    });
 
-	$('#btnPayment').click(function() {
-		current_item = this;
-		$('.section:visible').fadeOut( section_hide_time, function() {
-			$('a', '.mainmenu').removeClass( 'active' );
-			var new_section = $( $(current_item).attr('href') );
-			new_section.fadeIn( section_show_time );
-		} );
-	})
-    
-    $('#ofertalink').click(function() {
-    current_item = this;
-    $('.section:visible').fadeOut( section_hide_time, function() {
-        $('a', '.mainmenu').removeClass( 'active' );
-        var new_section = $( $(current_item).attr('href') );
-        new_section.fadeIn( section_show_time );
-    } );
+    //function setSection(aNewSection) {
+    //	$('.section:visible').fadeOut( section_hide_time, function() {
+    //		$('a', '.mainmenu').removeClass( 'active' );
+    //		var new_section = $( aNewSection );
+    //		new_section.fadeIn( section_show_time );
+    //	} );
+    //}
+    //
+    //$('#btnPayment').click(function() {
+    //	setSection($(this).attr('href'))
+    //});
+
+    $('.link-to-section').click(function () {
+        current_item = this;
+        $('.section:visible').fadeOut(section_hide_time, function () {
+            $('a', '.mainmenu').removeClass('active');
+            var new_section = $($(current_item).attr('href'));
+            new_section.fadeIn(section_show_time);
+        });
     })
 
     $('#btnPaid').click(function() {
